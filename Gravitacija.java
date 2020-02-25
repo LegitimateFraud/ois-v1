1498519f65e6f0acc50eb73b1a0b2b42fa89b3d5
LegitimateFraud <as4325@student.uni-lj.si>
2020-02-25 14:24:06 +0000

    prosim

--- Gravitacija.java
+++ Gravitacija.java
@@ -1,5 +1,8 @@
 public class Gravitacija {
     public static void main(String[] args) {
-        System.out.println("OIS je zakon!");
+        Scanner sc = new Scanner(System.in);
+        
+        double visina = sc.nextDouble();
+        
     }
 }
