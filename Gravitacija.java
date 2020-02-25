8b5f015d8bf912d80701b24b13177eed1ff966f2
xX-Smufa-69420-Xx <es6878@student.uni-lj.si>
2020-02-25 14:20:57 +0000

    Prosim

---
+++ Gravitacija.java
@@ -0,0 +1,8 @@
+public class Gravitacija {
+    public static void main(String[] args) {
+        public void Izpis(double nmv, double g) {
+            System.out.println(nmv);
+            System.out.println(g);
+        }
+    }
+}
