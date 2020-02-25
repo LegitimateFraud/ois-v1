43c76f9c26b5975cf9a452a766f5335d4850219d
YttriumTR <tr9212@student.uni-lj.si>
2020-02-25 14:16:13 +0000

    iz

--- Gravitacija.java
+++ Gravitacija.java
@@ -1,5 +1,12 @@
+import java.util.*;
 public class Gravitacija {
     public static void main(String[] args) {
-        System.out.println("OIS je zakon!");
+    }
+    public static double izracunG(double v){
+        float nadmorskavisina = sc.nextFloat();
+        double gravitacijskipospesek = 6,674* Math.pow(10,-11);
+        double masaZemlje = 5,972* Math.pow(10, 24);
+        double radijZemlje = 6,371 * Math.pow(10,6);
+        return ((gravitacijskipospesek*masaZemlje)/Math.pow((radijZemlje + v), 2));
     }
 }
